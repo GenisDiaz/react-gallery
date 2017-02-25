0d9a1ad1fd75e7ec0bcbb64649f2e808cf3889a0
GenisDiaz <genisdf89@gmail.com>
2017-02-25 03:20:46 +0100

    -add single component for view single photo

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single">
+        This is a single photo
+      </div>
+    )
+  }
+});
+
+export default Single;
--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 // import components
 import Main from './components/Main';
 import GalleryGrid from './components/GalleryGrid';
+import Single from './components/Single';
 
 //import router
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
@@ -11,7 +12,9 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 const router = (
   <Router history={browserHistory}>
     <Route path="/" component={Main}>
-      <IndexRoute component={GalleryGrid}></IndexRoute>
+      <IndexRoute component={GalleryGrid}>
+        <Route path="/view/:postId" component={Single}></Route>
+      </IndexRoute>
     </Route>
   </Router>
 )
