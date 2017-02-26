949caac4ef998d32b381105f939c31954dafabd0
GenisDiaz <genisdf89@gmail.com>
2017-02-26 03:06:32 +0100

    -add Photo component child of GalleryGrid

--- client/components/GalleryGrid.js
+++ client/components/GalleryGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Photo from './Photo';
 
 const GalleryGrid = React.createClass({
   render() {
     return (
       <div className="gallery-grid">
-        This is a gallery grid
+        {this.props.photos.map((photo, i) => <Photo {...this.props} key={i} i={i} photo={photo}/>)}
       </div>
     )
   }
---
+++ client/components/Photo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Photo = React.createClass({
+  render() {
+    return (
+      <figure className="grid-figure">>
+        This is a photo grid
+      </figure>
+    )
+  }
+});
+
+export default Photo;
