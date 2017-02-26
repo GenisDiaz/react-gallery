5ba12691ac288233579e6677d5b7d7a72e4ab7fe
GenisDiaz <genisdf89@gmail.com>
2017-02-26 04:22:06 +0100

    -add first iteration in fetch getPhoto initApp

--- client/util/photo.js
+++ client/util/photo.js
@@ -3,7 +3,7 @@ import config from '../data/config';
 // function for fetch get photos in init app
 export function getPhotos() {
   let fetchFlickr = fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a3aaff05f8fbe045216b9a991affd3ad&user_id=54694930@N05&per_page=5&format=json&nojsoncallback=1');
-  fetchFlickr.then( response => response)
+  fetchFlickr.then( response => response.json())
     .then(x => console.log('hola', x));
   return fetchFlickr;
 }
